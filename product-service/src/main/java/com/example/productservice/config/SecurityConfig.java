155b7cc42a784b123ea55048cf35cff82c2e3503
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-15 21:38:57 +0000

    oauth jwt verification with keycloak

--- product-service/src/main/java/com/example/productservice/config/SecurityConfig.java
+++ product-service/src/main/java/com/example/productservice/config/SecurityConfig.java
@@ -1,2 +1,23 @@
-package com.example.productservice.config;public class SecurityConfig {
-}
+//package com.example.productservice.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//public class SecurityConfig {
+//
+////    @Bean
+////    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+////        http
+////                .authorizeExchange()
+////                .pathMatchers()
+////                .permitAll()
+////                .anyExchange()
+////                .authenticated()
+////                .and()
+////                .oauth2ResourceServer()
+////                .jwt();
+////
+////                return http.build();
+////    }
+//}
